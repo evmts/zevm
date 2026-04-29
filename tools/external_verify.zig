be64314a8f2f8d572de000d7aaee9632cf115b1c
William Cory <willcory10@gmail.com>
2026-04-28 18:47:29 -0700

    external verify: expand legacy stRecursiveCreate

--- tools/external_verify.zig
+++ tools/external_verify.zig
@@ -43,6 +43,7 @@ const legacy_state_fixture_dirs = [_][]const u8{
     "ethereum-tests/LegacyTests/Cancun/GeneralStateTests/stAttackTest",
     "ethereum-tests/LegacyTests/Cancun/GeneralStateTests/stCallCodes",
     "ethereum-tests/LegacyTests/Cancun/GeneralStateTests/stChainId",
+    "ethereum-tests/LegacyTests/Cancun/GeneralStateTests/stRecursiveCreate",
     "ethereum-tests/LegacyTests/Cancun/GeneralStateTests/stSLoadTest",
 };
 
